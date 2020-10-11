bffbffa92f0fb9bc2f7c302a3e1d74296340a539
Ryan2144 <70672438+Ryan2144@users.noreply.github.com>
2020-10-10 19:19:41 -0500

    Create tiles above 1048576

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -48,8 +48,7 @@ HTMLActuator.prototype.addTile = function (tile) {
 
   // We can't use classlist because it somehow glitches when replacing classes
   var classes = ["tile", "tile-" + tile.value, positionClass];
-  if (tile.value > 1048576)
-  classes = ["tile", "tile-" + 1048576, positionClass];
+
   this.applyClasses(element, classes);
 
   element.textContent = tile.value;
